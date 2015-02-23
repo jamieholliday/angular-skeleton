addc117ca46dd7f3c8a66ee2c5176ddad3f052f3
Jamie Holliday <jamie@jamieholliday.com>
2015-02-23 20:49:24 +0000

    Update gulpfile. Remove bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,90 @@
-var gulp = require('gulp'),
-	refresh = require('gulp-livereload'),
-	sass = require('gulp-sass'),
-	connect = require('gulp-connect');
+;(function(){
+    'use strict';
+    
+    var gulp        = require('gulp'),
+        browserSync = require('browser-sync'),
+        reload      = browserSync.reload,
+        sass        = require('gulp-sass'),
+        minifyCss   = require('gulp-minify-css'),
+        concat      = require('gulp-concat'),
+        ngAnnotate  = require('gulp-ng-annotate'),
+        uglify      = require('gulp-uglify'),
+        minifyHtml  = require('gulp-minify-html'),
+        flatten     = require('gulp-flatten'),
+        sourceMaps  = require('gulp-sourcemaps'),
+        plumber     = require('gulp-plumber'),
+        paths;
+    
+    paths = {
+        js : [
+            'node_modules/angular/angular.js',
+            'node_modules/ui-router/angular-ui-router.js',
+            'app/main.js',
+            'app/modules/**/*.js'
+        ]
+    };
+        
 
-var paths = {
-	styles: {
-		src: 'public/modules/styles/**/*.scss',
-		dest: 'public/css'
-	},
-	html: {
-		src: ['public/index.html', 'public/partials/**/*.html']
-	},
-	js: {
-		src: 'public/modules/**/*.js'
-	}
-};
+    // Default task to be run with `gulp`
+    gulp.task('default', ['views', 'styles', 'js', 'browser-sync'], function () {
+        gulp.watch(['app/main.js', 'app/modules/**/*.js'], ['js']);
+            // Watch our scss files
+            gulp.watch(['app/main.scss', 'app/modules/**/*.scss'], [
+                'styles'
+            ]);
 
-gulp.task('default', ['connectDev', 'watch']);
+            gulp.watch(['app/index.html', 'app/modules/**/*.html'], [
+                'views'
+            ]);
+    });
 
-gulp.task('connectDev', function() {
-	connect.server({
-		livereload: true,
-		port: 8080
-	})
-});
+    // browser-sync task for starting the server.
+    gulp.task('browser-sync', function() {
+        browserSync({
+            server: {
+                baseDir: './public',
+                ghostMode: false
+            }
+        });
+    });
 
-gulp.task('watch', function() {
-	gulp.watch(paths.styles.src, ['styles']);
-	gulp.watch(paths.html.src, ['html']);
-	gulp.watch(paths.js.src, ['js']);
-});
+    // Styles
+    gulp.task('styles', function () {
+        return gulp.src('app/main.scss')
+            .pipe(sourceMaps.init())
+            .pipe(sass({
+                errorLogToConsole: true
+            }))
+            .pipe(sourceMaps.write())
+            .pipe(gulp.dest('public/'))
+            .pipe(reload({stream:true}));
+    });
 
-//runs sass task and live reload on scss changes
-gulp.task('styles', function() {
-	return gulp.src(paths.styles.src)
-		.pipe(sass({
-				sourceComments: 'map',
-				includePaths : [paths.styles.src]
-			}
-		))
-		.pipe(gulp.dest(paths.styles.dest))
-		.pipe(connect.reload());
-});
+    //JS
+    gulp.task('js', function() {
+        return gulp.src(paths.js)
+            .pipe(plumber())
+            .pipe(sourceMaps.init())
+            .pipe(concat('app.js'))
+            .pipe(sourceMaps.write())
+            .pipe(gulp.dest('public/'))
+            .pipe(reload({stream:true}));
+    });
 
-//runs livereload on html changes
-gulp.task('html', function() {
-	return gulp.src(paths.html.src)
-		.pipe(connect.reload());
-});
+    //Views
+    gulp.task('views', function() {
+        // Get our index.html
+        gulp.src('app/index.html')
+            .pipe(plumber())
+            // And put it in the public folder
+            .pipe(gulp.dest('public/'))
+            .pipe(reload({stream:true}));
 
-//runs livereload on all js changes
-gulp.task('js', function() {
-	return gulp.src(paths.js.src)
-	.pipe(connect.reload());
-});
+            // Any other view files from app/views
+            gulp.src('app/modules/**/*.html')
+            // Will be put in the public/views folder
+            .pipe(flatten())
+            .pipe(gulp.dest('public/views/'))
+            .pipe(reload({stream:true}));
+    });
+})();
